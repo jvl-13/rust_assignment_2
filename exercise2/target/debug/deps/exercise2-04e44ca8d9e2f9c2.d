be13a25844976e6f53415d31fc533ea4dbb5cce5
LaiVoJM <84391289+LaiVoJM@users.noreply.github.com>
2022-10-22 01:27:07 +0700

    done

---
+++ exercise2/target/debug/deps/exercise2-04e44ca8d9e2f9c2.d
@@ -0,0 +1,5 @@
+d:\LaiVo\Rust\rust_assignment_2\exercise2\target\debug\deps\exercise2-04e44ca8d9e2f9c2.rmeta: src\main.rs
+
+d:\LaiVo\Rust\rust_assignment_2\exercise2\target\debug\deps\exercise2-04e44ca8d9e2f9c2.d: src\main.rs
+
+src\main.rs:
---
+++ exercise2/target/debug/deps/exercise2-7d0fa48d9608fb00.d
@@ -0,0 +1,5 @@
+d:\LaiVo\Rust\rust_assignment_2\exercise2\target\debug\deps\exercise2-7d0fa48d9608fb00.rmeta: src\main.rs
+
+d:\LaiVo\Rust\rust_assignment_2\exercise2\target\debug\deps\exercise2-7d0fa48d9608fb00.d: src\main.rs
+
+src\main.rs:
